90f53e0bf9d73970bd2ff2710fe5e05e8c7c6655
Matt Hannan <matthannan1@gmail.com>
2017-04-19 22:07:42 -0400

    ex33

---
+++ ex33.py
@@ -0,0 +1,25 @@
+
+#numbers = []
+
+def loop(val,incrementor):
+    i = 0
+    numbers = []
+    print val
+    while i < val:
+        print "At the top, i is %d and val is %d" % (i, val)
+
+        numbers.append(i)
+        i = i + incrementor
+        print i < val
+        print "Nmbers now: ", numbers
+        print "At the bottom, i is %d and val is %d" % (i, val)
+        print
+    print "The numbers: "
+    for num in numbers:
+        print num
+
+input = raw_input("Enter a low number: ")
+val = int(input)
+input2 = raw_input("Enter amount to increase the number by: ")
+incrementor = int(input2)
+loop(val,incrementor)
