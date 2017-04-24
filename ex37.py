52750f759398c40ea99130211e2a88254a1e5fd5
Matt Hannan <matthannan1@gmail.com>
2017-04-24 15:45:45 -0400

    ex37

---
+++ ex37.py
@@ -0,0 +1,8 @@
+# ex37.py is to test out a little of this and a little of that.
+# The list is fairly lengthy.
+
+# and
+a = 1
+b = 1
+if a and b:
+    print "a is %d and b is %d. They are equal." % (a, b)
